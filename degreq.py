a6e6ced567b33af00f1e4426e9570a9f3de8a906
Chase Strickler <stricklc@augsburg.edu>
2018-11-13 18:06:26 -0600

    Moved stuff from main to function 'create_major'

--- degreq.py
+++ degreq.py
@@ -9,16 +9,16 @@ class DegreeRequirements:
 		self.name = major
 		self.courses = classes.copy()
 
-	def print_arr(self, list):
-		"""
-		Prints out the list
-		"""
-		for i in range(len(list)-1):
-			print("----------", list[i].name, "----------")
-			print()
-			for j in range(len(list[i].courses)):
-				print(list[i].courses[j])
-			print()
+def print_arr(list):
+	"""
+	Prints out the list
+	"""
+	for i in range(len(list)-1):
+		print("----------", list[i].name, "----------")
+		print()
+		for j in range(len(list[i].courses)):
+			print(list[i].courses[j])
+		print()
 
 def get_site(url):
 	page_response = requests.get(url, timeout=5)
@@ -31,6 +31,29 @@ def get_site(url):
 def create_url(str):
 
 	return("http://www.augsburg.edu/" + str + "/degree-requirements")
+
+def create_major(majors, titles, degrees, uls):
+	for k in range(len(titles)-1): #Only adds item to the major list if it has the word major or minor in it
+		if "Major" in str(titles[k]):
+			#print("Appended: ", titles[k])
+			this_ul = []
+			children = uls.findChildren("li", recursive=False)
+			
+			for child in children: #Adds children of a ul tag to an array
+				this_ul.append(str(child.text.strip().replace('<li>', '')))
+			degree_requirements = DegreeRequirements(titles[k], this_ul) #instance of class
+			degrees.append(degree_requirements)	#add degree_requirements object to list
+
+		if "Minor" in str(titles[k]):
+			#print("Appended: ", titles[k])
+			this_ul = []
+			children = uls.findChildren("li", recursive=False)
+			
+			for child in children:
+				this_ul.append(str(child.text.strip().replace('<li>', '')))
+			degree_requirements = DegreeRequirements(titles[k], this_ul)
+			degrees.append(degree_requirements)
+
 
 def main():
 	#Normal degrees hold the part of the URL for degrees that have similar HTML set up. The URL for all of these is www.augsburg.edu/{INSERT_COURSE}/degree-requirements
@@ -45,9 +68,11 @@ def main():
 	#Medieval Studies ends in /program-details/
 
 	for i in range(len(normal_degrees)-1): #loops for all degrees
+			
+		#if "biology" in 
 		url = create_url(normal_degrees[i])
-		
-		#properly appends the URL for a major
+	
+				#properly appends the URL for a major
 		majors = get_site(url)
 		#finds the proper div
 
@@ -58,34 +83,39 @@ def main():
 		titles = []
 		uls = majors.ul #grabs all of the ul tags on the page
 		uls.text
-		#print(uls.text)
-		
+
 		for a in x: #This loop strips the list of all h2 tags
 			titles.append(str(a.text.strip().replace('<h2>', '').replace('</h2>', '')))
 
+		create_major(majors, titles, degrees, uls)
+		#print(uls.text)
+		
+		# for a in x: #This loop strips the list of all h2 tags
+		# 	titles.append(str(a.text.strip().replace('<h2>', '').replace('</h2>', '')))
 
-		for k in range(len(titles)-1): #Only adds item to the major list if it has the word major or minor in it
-			if "Major" in str(titles[k]):
-				#print("Appended: ", titles[k])
-				this_ul = []
-				children = uls.findChildren("li", recursive=False)
+
+		# for k in range(len(titles)-1): #Only adds item to the major list if it has the word major or minor in it
+		# 	if "Major" in str(titles[k]):
+		# 		#print("Appended: ", titles[k])
+		# 		this_ul = []
+		# 		children = uls.findChildren("li", recursive=False)
 				
-				for child in children: #Adds children of a ul tag to an array
-					this_ul.append(str(child.text.strip().replace('<li>', '')))
-				degree_requirements = DegreeRequirements(titles[k], this_ul) #instance of class
-				degrees.append(degree_requirements)	#add degree_requirements object to list
+		# 		for child in children: #Adds children of a ul tag to an array
+		# 			this_ul.append(str(child.text.strip().replace('<li>', '')))
+		# 		degree_requirements = DegreeRequirements(titles[k], this_ul) #instance of class
+		# 		degrees.append(degree_requirements)	#add degree_requirements object to list
 
-			if "Minor" in str(titles[k]):
-				#print("Appended: ", titles[k])
-				this_ul = []
-				children = uls.findChildren("li", recursive=False)
+		# 	if "Minor" in str(titles[k]):
+		# 		#print("Appended: ", titles[k])
+		# 		this_ul = []
+		# 		children = uls.findChildren("li", recursive=False)
 				
-				for child in children:
-					this_ul.append(str(child.text.strip().replace('<li>', '')))
-				degree_requirements = DegreeRequirements(titles[k], this_ul)
-				degrees.append(degree_requirements)				
+		# 		for child in children:
+		# 			this_ul.append(str(child.text.strip().replace('<li>', '')))
+		# 		degree_requirements = DegreeRequirements(titles[k], this_ul)
+		# 		degrees.append(degree_requirements)				
 
-	degree_requirements.print_arr(degrees)
+	print_arr(degrees)
 
 if __name__ == '__main__':
 	main()  
