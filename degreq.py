de8e89f3e25dcd08e2dcdf26c93a134f1965ff02
Chase <cstrickler01@hamlin.edu>
2018-11-30 14:25:01 -0600

    Added comments

--- degreq.py
+++ degreq.py
@@ -5,6 +5,9 @@ import sys
 import csv
 
 class DegreeRequirements:
+	"""
+	This class creates degree objects which hold the name of the major and a list of the courses required to complete that degree.
+	"""
 
 	def __init__(self, major, classes):
 		self.name = major
@@ -69,11 +72,14 @@ def create_url(str):
 		return("http://www.augsburg.edu/" + str + "/degree-requirements")
 
 def create_major(majors, titles, degrees, uls, majorkeyword, minorkeyword):
+	"""
+	Creates a major object by checking if a given keyword is in the h2 string.
+	If so, grab the next unordered list as the courses and strip all of the tags from the text.
+	"""
+
 	for k in titles: #Only adds item to the major list if it has the word major or minor in it
 		if "Music Core" in str(k):
 			majorkeyword = "Core"
-		else:
-			majorkeyword = "Major"
 			
 		if majorkeyword in str(k):
 			#print(str(k))
@@ -100,7 +106,7 @@ def create_major(majors, titles, degrees, uls, majorkeyword, minorkeyword):
 def main():
 	#Normal degrees hold the part of the URL for degrees that have similar HTML set up. The URL for all of these is www.augsburg.edu/{INSERT_COURSE}/degree-requirements
 	#Psychology doesn't account for any concentrations
-	normal_degrees = ["ais", "art", 
+	url_segments = ["ais", "art", 
 					  "biology/degrees/ba-biology/", "biology/degrees/bs-biology/", "biology/degrees/ba-life-sciences/", "biology/degrees/biopsychology/", 
 					  "business/degree-requirements/business-administration/", "business/degree-requirements/accounting/", "business/degree-requirements/finance/", "business/degree-requirements/international-business/", "business/degree-requirements/management/", "business/degree-requirements/management-information-systems/", "business/degree-requirements/marketing/", 
 					  "communication/degrees/communication-studies/", "communication/degrees/film/", "communication/degrees/new-media/",
@@ -114,42 +120,42 @@ def main():
 	#Film does not account for tracks. See: http://www.augsburg.edu/communication/degrees/film/
 	#Education major is suuuuuuper wacky and not at all uniform. Special education has been accounted for, but I'm not at all sure how to do the rest.
 
-	for i in range(len(normal_degrees)):
-		url = create_url(normal_degrees[i])
+	for i in range(len(url_segments)): # assign keyword based on the url segment to help parse the h2 elemenents in the create_major function
+		url = create_url(url_segments[i])
 
 		majorkeyword = "Major"
 		minorkeyword = "Minor"
 
-		if "biology" in normal_degrees[i]:
+		if "biology" in url_segments[i]:
 			majorkeyword = "Bachelor"
 			minorkeyword = "Bachelor"
 
-		if "music" in normal_degrees[i]:
-			majorkeyword = "Major"
-			minorkeyword = "Minor"	
+		# if "music" in url_segments[i]:
+		# 	majorkeyword = "Major"
+		# 	minorkeyword = "Minor"	
 
 		#properly appends the URL for a major
 		majors = get_site(url)
 		#finds the proper div
 		x = [];
 
-		if "/" in normal_degrees[i]:
-			if "business" in normal_degrees[i]:
+		if "/" in url_segments[i]:
+			if "business" in url_segments[i]:
 				x = majors.find_all('h3')
 
-			if "communication" in normal_degrees[i]:
+			if "communication" in url_segments[i]:
 				x = majors.find_all('h3')
 
-			if "education" in normal_degrees[i]:
+			if "education" in url_segments[i]:
 				x = majors.find_all('h3')
 
-			if "biology" in normal_degrees[i]:
+			if "biology" in url_segments[i]:
 				page_response = requests.get(url, timeout=5)
 				page_content = BeautifulSoup(page_response.content, "html.parser")
 				x = page_content.find(class_='entry-title')
-			if "music" in normal_degrees[i]:
+			if "music" in url_segments[i]:
 				x = majors.find_all('h2')
-			if "medievalstudies" in normal_degrees[i]:
+			if "medievalstudies" in url_segments[i]:
 				x = majors.find_all('h2')
 
 		else:
@@ -160,21 +166,21 @@ def main():
 		uls.text
 
 		for a in x: #This loop strips the list of all h2 tags
-			if "/" in normal_degrees[i]:
-				if "business" in normal_degrees[i]:
+			if "/" in url_segments[i]:
+				if "business" in url_segments[i]:
 					titles.append(str(a.text.strip().replace('<h3>', '').replace('</h3>', '')))
 			#		print(a.text)
-				if "communication" in normal_degrees[i]:
+				if "communication" in url_segments[i]:
 					titles.append(str(a.text.strip().replace('<h3>', '').replace('</h3>', '')))
-				if "education" in normal_degrees[i]:
+				if "education" in url_segments[i]:
 					titles.append(str(a.text.strip().replace('<h3>', '').replace('</h3>', '')))
-				if "biology" in normal_degrees[i]:
+				if "biology" in url_segments[i]:
 				 	titles.append(str(a.strip().replace('<h1 class="entry-title">', '').replace('</h1>', '')))
 				 	#print(str(titles[0]))
-				if "music" in normal_degrees[i]:
+				if "music" in url_segments[i]:
 					titles.append(str(a.text.strip().replace('<h2>', '').replace('</h2>', '')))
 				#	print(a.text)
-				if "medievalstudies" in normal_degrees[i]:
+				if "medievalstudies" in url_segments[i]:
 					titles.append(str(a.text.strip().replace('<h2>', '').replace('</h2>', '')))
 			else:
 				titles.append(str(a.text.strip().replace('<h2>', '').replace('</h2>', '')))
